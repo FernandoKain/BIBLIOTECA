f33437088558a8124c9eefa71f3c01d08e546f50
FernandoKain <fernandomanga@hotmail.com>
2023-10-17 23:35:39 -0300

    Implementando o banco de dados

--- usuarios/admin.py
+++ usuarios/admin.py
@@ -1,3 +1,8 @@
+from usuarios.models import Usuario
 from django.contrib import admin
 
 # Register your models here.
+
+@admin.register(Usuario)
+class UsuarioAdmin(admin.ModelAdmin):
+    readonly_fields = ('nome', 'email', 'senha')
--- usuarios/models.py
+++ usuarios/models.py
@@ -1,6 +1,6 @@
 from django.db import models
-from django.http import HttpResponse
-from .models import Usuario
+
+
 
 # Create your models here.
 class Usuario(models.Model):
